669dbd1c865a10ca88f76e7ca5226ab50258639d
MridulHu <mriduldas0812@gmail.com>
2024-08-31 23:09:37 +0530

    fixed contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       <h1>Contact Me</h1>
       <div className="contact-info">
         <p><strong>Email:</strong> mriduldas0812@gmail.com</p>
-        <p><strong>Phone:</strong> +917000552842</p>
+        <p><strong>Instagram:</strong> <a href="https://www.instagram.com/mridul._das?igsh=M2N2bXhsa3V3czA4" target="_blank" rel="noopener noreferrer">@mridul._das</a></p>
         <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/mriduldas08/" target="_blank" rel="noopener noreferrer">linkedin.com/in/mridulhu</a></p>
         <p><strong>GitHub:</strong> <a href="https://github.com/MridulHu" target="_blank" rel="noopener noreferrer">github.com/MridulHu</a></p>
       </div>
